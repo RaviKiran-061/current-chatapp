48d101cd4bf66812706fdac2be2e8ef7b8a329e9
RaviKiran Singh <asiskumar_singh@hotmail.com>
2025-11-22 00:00:38 +0545

    added env helper function file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
-import dotenv from 'dotenv';
 import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
+import { ENV } from './lib/env.js'
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
@@ -19,7 +19,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes)
 
 // static frontend for deployment
-if (process.env.NODE_ENV === 'production') {
+if (ENV.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
     app.get((_, res) => {
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === 'production') {
 
 
 app.listen(5000, ()=> {
-    console.log(`Server is running on PORT: ${process.env.PORT}`) 
+    console.log(`Server is running on PORT: ${ENV.PORT}`) 
     connectDB();
 });
