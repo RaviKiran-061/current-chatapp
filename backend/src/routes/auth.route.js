d907de5b04982eff09ce706f1bd7cea7571c9d36
RaviKiran Singh <asiskumar_singh@hotmail.com>
2025-11-23 00:00:08 +0545

    rate-limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,8 +2,13 @@ import express from 'express';
 
 import { protectRoute } from '../middlewares/auth.middleware.js';
 import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { arcjetProtection } from '../middlewares/arcjet.middleware.js';
 
 const router = express();
+
+// this middleware first runs on every request
+// if passes only runs next function on it
+router.use(arcjetProtection);
 
 router.post('/signup', signup);
 router.post('/login', login);
